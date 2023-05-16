e034766cdbb83610c0daca336408464f432ba3da
menem <menem@DESKTOP-8V5FLBK>
2023-05-16 15:51:51 -0300

    Agregar archivos de proyecto.

---
+++ pryMascota/frmMain.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace pryMascota
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnAlimentar = new System.Windows.Forms.Button();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnCuidar = new System.Windows.Forms.Button();
+            this.bntSalir = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lstTipo = new System.Windows.Forms.ComboBox();
+            this.listMascotas = new System.Windows.Forms.ListBox();
+            this.ptMascotas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptMascotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(36, 35);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(36, 76);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(32, 13);
+            this.lblEdad.TabIndex = 1;
+            this.lblEdad.Text = "Edad";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(36, 119);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(76, 13);
+            this.lblTipo.TabIndex = 2;
+            this.lblTipo.Text = "Tipo de animal";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(37, 156);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnAlimentar
+            // 
+            this.btnAlimentar.Location = new System.Drawing.Point(37, 302);
+            this.btnAlimentar.Name = "btnAlimentar";
+            this.btnAlimentar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlimentar.TabIndex = 4;
+            this.btnAlimentar.Text = "Alimentar";
+            this.btnAlimentar.UseVisualStyleBackColor = true;
+            this.btnAlimentar.Click += new System.EventHandler(this.btnAlimentar_Click);
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(37, 357);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(75, 23);
+            this.btnJugar.TabIndex = 5;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // btnCuidar
+            // 
+            this.btnCuidar.Location = new System.Drawing.Point(140, 302);
+            this.btnCuidar.Name = "btnCuidar";
+            this.btnCuidar.Size = new System.Drawing.Size(75, 23);
+            this.btnCuidar.TabIndex = 6;
+            this.btnCuidar.Text = "Cuidar";
+            this.btnCuidar.UseVisualStyleBackColor = true;
+            this.btnCuidar.Click += new System.EventHandler(this.btnCuidar_Click);
+            // 
+            // bntSalir
+            // 
+            this.bntSalir.Location = new System.Drawing.Point(140, 357);
+            this.bntSalir.Name = "bntSalir";
+            this.bntSalir.Size = new System.Drawing.Size(75, 23);
+            this.bntSalir.TabIndex = 7;
+            this.bntSalir.Text = "Salir";
+            this.bntSalir.UseVisualStyleBackColor = true;
+            this.bntSalir.Click += new System.EventHandler(this.bntSalir_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(132, 35);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(83, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(132, 76);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(83, 20);
+            this.txtEdad.TabIndex = 9;
+            // 
+            // lstTipo
+            // 
+            this.lstTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstTipo.FormattingEnabled = true;
+            this.lstTipo.Items.AddRange(new object[] {
+            "Perro ",
+            "Gato",
+            "Tortuga",
+            "Hamster"});
+            this.lstTipo.Location = new System.Drawing.Point(132, 119);
+            this.lstTipo.Name = "lstTipo";
+            this.lstTipo.Size = new System.Drawing.Size(83, 21);
+            this.lstTipo.TabIndex = 10;
+            // 
+            // listMascotas
+            // 
+            this.listMascotas.FormattingEnabled = true;
+            this.listMascotas.Location = new System.Drawing.Point(37, 185);
+            this.listMascotas.Name = "listMascotas";
+            this.listMascotas.Size = new System.Drawing.Size(178, 108);
+            this.listMascotas.TabIndex = 11;
+            this.listMascotas.SelectedIndexChanged += new System.EventHandler(this.listMascotas_SelectedIndexChanged);
+            // 
+            // ptMascotas
+            // 
+            this.ptMascotas.Location = new System.Drawing.Point(237, 185);
+            this.ptMascotas.Name = "ptMascotas";
+            this.ptMascotas.Size = new System.Drawing.Size(167, 108);
+            this.ptMascotas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptMascotas.TabIndex = 12;
+            this.ptMascotas.TabStop = false;
+            this.ptMascotas.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 396);
+            this.Controls.Add(this.ptMascotas);
+            this.Controls.Add(this.listMascotas);
+            this.Controls.Add(this.lstTipo);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.bntSalir);
+            this.Controls.Add(this.btnCuidar);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.btnAlimentar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Veterinaria";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptMascotas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnAlimentar;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnCuidar;
+        private System.Windows.Forms.Button bntSalir;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.ComboBox lstTipo;
+        private System.Windows.Forms.ListBox listMascotas;
+        private System.Windows.Forms.PictureBox ptMascotas;
+    }
+}
+
---
+++ pryMascota/frmMain.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMascota
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listMascotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listMascotas.Text == "Perro")
+            {
+
+                ptMascotas.Image = Image.FromFile("perro.jpeg");
+                lblTipo.Visible = true;
+                lblEdad.Visible = true;
+
+            }
+            else
+            {
+                if (listMascotas.Text == "Gato")
+                {
+                    ptMascotas.Image = Image.FromFile("gato.jpg");
+                    lblTipo.Visible = true;
+                    lblEdad.Visible = true;
+                }
+                else
+                {
+                    if (listMascotas.Text == "Tortuga")
+                    {
+                        ptMascotas.Image = Image.FromFile("Tortuga-de-tierra.jpg");
+                        lblTipo.Visible = true;
+                        lblEdad.Visible = true;
+                    }
+                    else
+                    {
+                        if (listMascotas.Text == "Hamster")
+                        {
+                            ptMascotas.Image = Image.FromFile("hamster.jpeg");
+                            lblTipo.Visible = true;
+                            lblEdad.Visible = true;
+                        }
+                        
+                    }
+                   
+                }
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string[] GuardarNombre = new string[10];
+
+            ClsMascota NuevaMascota = new ClsMascota();
+            NuevaMascota.nombre = txtNombre.Text;
+            NuevaMascota.edad = txtEdad.Text;
+            NuevaMascota.tipoAnimal = lstTipo.Text;
+            List<ClsMascota> ListarMascotas = new List<ClsMascota>();
+            ListarMascotas.Add(NuevaMascota);
+
+            foreach (var item in ListarMascotas)
+            {
+                listMascotas.Items.Add(item.tipoAnimal);
+            }
+
+            txtNombre.Clear();
+            txtEdad.Clear();
+            lstTipo.SelectedIndex = -1;
+
+        }
+
+        private void btnCuidar_Click(object sender, EventArgs e)
+        {
+            ClsMascota Cuidar = new ClsMascota();
+            Cuidar.nombre = listMascotas.SelectedItem.ToString();
+            MessageBox.Show(Cuidar.CuidarMascota());
+        }
+
+        private void btnAlimentar_Click(object sender, EventArgs e)
+        {
+            ClsMascota Alimentar = new ClsMascota();
+            Alimentar.nombre = listMascotas.SelectedItem.ToString();
+            MessageBox.Show(Alimentar.AlimentarMascota());
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            ClsMascota Jugar = new ClsMascota();
+            Jugar.nombre = listMascotas.SelectedItem.ToString();
+            MessageBox.Show(Jugar.JugarMascota());
+        }
+
+        private void bntSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
